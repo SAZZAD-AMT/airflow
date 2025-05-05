802cadebc9cd38f5c3ce6402184fdaece46b75ff
Sazzad Hossen <80639629+SAZZAD-AMT@users.noreply.github.com>
2025-05-05 06:53:42 +0000

    done

--- airflow_dag/dags/Branch_Example.py
+++ airflow_dag/dags/Branch_Example.py
@@ -1,9 +1,8 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import DummyOperator  # Updated import for Airflow 3.0
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.utils.dates import days_ago
 from airflow.utils.trigger_rule import TriggerRule
-
 
 def notification_email(**kwargs):
     if kwargs['ti'].xcom_pull(task_ids='check_data'):
@@ -13,9 +12,7 @@ def notification_email(**kwargs):
 
 def always_true():
     return True
-    
 
-        
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
@@ -25,10 +22,10 @@ dag = DAG(
     'branch_example',
     default_args=default_args,
     schedule_interval=None,
+    catchup=False  # Recommended to add
 )
 
 start_task = DummyOperator(task_id='start_task', dag=dag)
-
 
 check_data = PythonOperator(
     task_id='check_data',
@@ -36,21 +33,18 @@ check_data = PythonOperator(
     dag=dag,
 )
 
-# Updated to use BranchPythonOperator
 branch_task = BranchPythonOperator(
     task_id='branch_task',
     python_callable=notification_email,
-    provide_context=True,
     dag=dag,
 )
 
-
 dummy_task_true = DummyOperator(task_id='dummy_task_true', dag=dag)
-
 dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+dummy_task_final = DummyOperator(
+    task_id='dummy_task_final',
+    trigger_rule=TriggerRule.ONE_SUCCESS,
+    dag=dag
+)
 
-dummy_task_final = DummyOperator(task_id='dummy_task_final', trigger_rule=TriggerRule.ONE_SUCCESS, dag=dag)
-
-start_task >> check_data >> branch_task >> [dummy_task_true, dummy_task]>>dummy_task_final 
-
-
+start_task >> check_data >> branch_task >> [dummy_task_true, dummy_task] >> dummy_task_final
