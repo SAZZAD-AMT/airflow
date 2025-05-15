2298bc2f6466e0b4f3d516a0bac27e4d349ed9b6
Walton@GLA#2024 <sbd.dsa5@waltonplc.com>
2025-05-15 18:35:47 +0600

    Create minio_test.py

---
+++ airflow_dag/dags/minio_test.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+def upload_to_minio():
+    hook = S3Hook(aws_conn_id='minio_default')
+    hook.load_string(
+        string_data="Hello from Airflow!",
+        key='test_file.txt',
+        bucket_name='airflow'
+    )
+
+with DAG(
+    'minio_example',
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    
+    upload_task = PythonOperator(
+        task_id='upload_to_minio',
+        python_callable=upload_to_minio
+    )
